b027f049040273c9e220764da5faa82811fca8da
m-saad-0 <msad8961@gmail.com>
2023-10-15 12:24:23 +0500

    It works great.

---
+++ script.js
@@ -0,0 +1,102 @@
+let choices = ["rock", "paper", "scissors"];
+let resultRecords = [0,0];
+let playerChoice = "";
+let computerChoice = ""
+let wlClass = ["", ""]
+
+const winner = () => {
+    computerChoice = choices[Math.trunc(Math.random()*3)];
+    announceResult();
+    return playerChoice === "rock" && computerChoice === "paper"? "c" 
+            : playerChoice === "paper" && computerChoice === "scissors"? "c" 
+            : playerChoice === "scissors" && computerChoice === "rock"? "c" 
+            : playerChoice === computerChoice? "t" : "p";
+}
+
+const runAndDeclareResults = () => {
+    let result = winner();
+    const pl = document.querySelector(".user");
+    const comp = document.querySelector(".computer");
+    if(wlClass[0]){
+    pl.classList.remove(wlClass[0]);
+    comp.classList.remove(wlClass[1]);
+    }
+    if(result === "p")
+    {
+        wlClass[0] = "w";
+        wlClass[1] = "l"
+        pl.classList.add("w")
+
+        comp.classList.add("l")
+
+        resultRecords[0]+=1;
+
+        const changeNumber = document.querySelector(".player > p");
+        changeNumber.innerHTML = `${resultRecords[0]}`;
+    }
+    else if(result === "c")
+    {
+        wlClass[1] = "w";
+        wlClass[0] = "l"
+        pl.classList.add("l");
+
+        comp.classList.add("w");
+
+        resultRecords[1]+=1;
+        const changeNumber = document.querySelector(".comp > p");
+        changeNumber.innerHTML = `${resultRecords[1]}`;
+    }
+}
+
+const rounds = ()=>{
+    if(resultRecords[0]>=5)
+    {
+        const text = document.querySelector(".player > h3");
+        text.innerHTML = "Player won!";
+        return 1;
+    }
+    else if(resultRecords[1]>=5)
+    {
+        const text = document.querySelector(".comp > h3");
+        text.innerHTML = "Computer won!";
+        return 1;
+    }
+    return 0;
+}
+
+const announceResult = () => {
+    console.log("called")
+    const sayIt = document.querySelector(".results > .r");
+    console.log(sayIt)
+    const listElement = document.createElement("li");
+    let message = "";
+    if(playerChoice===computerChoice) message = "It is a tie."
+    else message = `You chose ${playerChoice} and the computer chose ${computerChoice}.`;
+    listElement.innerHTML = message;
+    sayIt.appendChild(listElement);
+}
+
+const checkRoundsandReplay = ()=> {
+    let r = rounds();
+    if(r) {
+        const b = document.querySelector(".button > button");
+        b.classList.add("tryagain");
+        b.innerHTML = "Play Again"
+        b.classList.remove("start");
+    }
+}
+const initGame = () => {
+    runAndDeclareResults();
+    checkRoundsandReplay();
+}
+
+const b = document.querySelector(".button > button");
+if(b.classList[0] === ("tryagain"))
+    location.reload();
+
+const playerDiv = document.querySelector(".imgs");
+
+playerDiv.addEventListener('click', (e)=>{
+            playerChoice = e.target.classList[1];
+            if(resultRecords[1]!==5 && resultRecords[0]!==5) initGame();
+        })
